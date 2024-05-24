8ae14daf6ffb11398e5bfacc16035e95c6141705
Soundleo <leorperes@gmail.com>
2024-05-24 20:17:25 -0300

    lista de fruta versão 1.0

---
+++ main.py
@@ -0,0 +1,25 @@
+# criar uma lista de frutas
+frutas = []
+import os
+from datetime import date
+dia =   date.today().day
+
+print ('*******************lista de compras********************')
+
+while True:
+    # inserindo uma nova prouta 
+    nova_fruta = input("informe o nome da nova fruta ou deixe vazio para encerrar  \n").capitalize()
+
+    if nova_fruta != '':
+        # inserindo o nome na lista
+        frutas.append(nova_fruta)
+        continue
+    else: 
+        break
+
+
+# exibir na tela a lista
+os.system('cls')
+for fruta in frutas:
+    print(fruta)
+print(dia)
